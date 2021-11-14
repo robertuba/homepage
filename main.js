996ad3ca5c6794ce02dd4e9290a0081e46a8f7c8
Robert Uba <robertuba888@gmail.com>
2021-11-14 08:32:46 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const name='Robert'
+const age = 33
+
+//alert(`Siemanderko na RObakowatej stronce!`);
+
+console.log(`Mam na imię ${name} i mam ${age} lata i w tym wieku chcę się nałuczyć programować!`);
